aecd3d518cb450cd0a170d8b786560e649f68938
Patrick S. Young <patrick@bigeyeball.co>
2021-01-16 00:34:43 -0800

    update <MosaicTiles/> graphics to v2.0.0

--- mosaic-frontend/src/features/mosaicVideo/helpers/index.ts
+++ mosaic-frontend/src/features/mosaicVideo/helpers/index.ts
@@ -2,4 +2,3 @@ export { getCopyVideoFromArea } from 'features/mosaicVideo/helpers/getCopyVideoF
 export { getInPoints } from 'features/mosaicVideo/helpers/getInPoints';
 export { getDrawToCanvasArea } from 'features/mosaicVideo/helpers/getDrawToCanvasArea';
 export { getTileAnimEvents } from 'features/mosaicVideo/helpers/getTileAnimEvents';
-export { getMosaicSelectorConfig } from 'features/mosaicVideo/helpers/getMosaicSelectorConfig';
