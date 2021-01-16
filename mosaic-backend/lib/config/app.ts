067c9a424f238d141de102464af28bb0b1662fbb
Patrick S. Young <patrick@bigeyeball.co>
2021-01-16 00:32:32 -0800

    add cors to express configuration

--- mosaic-backend/lib/config/app.ts
+++ mosaic-backend/lib/config/app.ts
@@ -27,7 +27,6 @@ class App {
 
    private config(): void {
       this.app.use(bodyParser.json());
-      this.app.use(cors());
       this.app.use(bodyParser.urlencoded({ extended: false }));
       this.app.use(expressFileUpload());
       this.app.use('/uploads', express.static(`${env.getVolumnPath()}`));
