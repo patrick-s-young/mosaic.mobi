89c994714853107e4925bb26ef58d743e4e774c0
Patrick S. Young <patrick@bigeyeball.co>
2021-02-11 20:29:54 -0800

    Add <PopOver> react-spring enabled component

---
+++ mosaic-frontend/src/components/PopOver.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {useSpring, animated} from 'react-spring';
+
+export interface PopOverProps {
+  width: string,
+  height: string,
+  showTop: string
+  hideTop: string
+  isActive: boolean
+}
+
+const PopOver: React.FC<PopOverProps> = ({
+  children,
+  width,
+  height,
+  showTop,
+  hideTop,
+  isActive }) => {
+  const slideProps = useSpring({
+    from: { 
+      top: isActive ? hideTop : showTop
+    },
+    to: {
+      top: isActive ? showTop : hideTop
+    }
+  });
+
+  return (
+    <div style={{ 
+      position: 'absolute', 
+      overflow: 'hidden',
+      marginTop: '0',
+      width,  
+      height, 
+      display: 'block' }}
+    >
+    <animated.div
+      style={{
+        ...slideProps,
+        position: 'absolute',
+        display: 'block'
+      }}
+    >
+      {children}
+    </animated.div>
+    </div>
+  );
+}
+
+export default PopOver;
