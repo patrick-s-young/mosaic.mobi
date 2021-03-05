7cc80f91e973defeb435e94794b2e32a2ef58db1
Patrick S. Young <patrick@bigeyeball.co>
2021-03-05 09:08:07 -0800

    migrate css to jss to implement custom background color

--- mosaic-frontend/src/devTools/devicePreview/DevicePreview.tsx
+++ mosaic-frontend/src/devTools/devicePreview/DevicePreview.tsx
@@ -1,15 +1,25 @@
 import * as React from 'react';
 import iPhone_XR from '../../assets/device-preview/iPhone_XR_mock_414x712.png';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => 
+  createStyles({
+    appContainer: {
+      backgroundColor: theme.palette.common.white
+    },
+  })
+);
 
 export type DevicePreviewProps = {
   children: Array<React.ReactChild>
 }
 
 const DevicePreview: React.FC<DevicePreviewProps> = ({ children }) => {
+  const classes = useStyles();
   return (
     <div style={{ padding: '2%', marginLeft: '200px'}}>
         <img src={iPhone_XR} style={{ position: 'absolute'}} id='device-png'/>
-        <div style={{ position: 'absolute', marginTop: '132px', marginLeft: '35px', backgroundColor: 'white'}}>
+        <div style={{ position: 'absolute', marginTop: '132px', marginLeft: '35px'}} className={classes.appContainer}>
           {children}
         </div>
     </div>
