20a7cab0680327446b494493257455db05f6a542
Patrick S. Young <patrick@bigeyeball.co>
2021-03-06 12:47:53 -0800

    add add coming soon copy and grid layout adjustments

--- mosaic-frontend/src/devTools/devicePreview/DevicePreview.tsx
+++ mosaic-frontend/src/devTools/devicePreview/DevicePreview.tsx
@@ -1,12 +1,35 @@
 import * as React from 'react';
 import iPhone_XR from '../../assets/device-preview/iPhone_XR_mock_414x712.png';
 import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import { Paper, Grid } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) => 
   createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    gridLeft: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100%'
+    },
     appContainer: {
       backgroundColor: theme.palette.common.white
     },
+    headline: {
+      color: theme.palette.primary.dark,
+      ...theme.typography.h3
+    },
+    paper: {
+      textAlign: 'left',
+      color: '#555',
+      backgroundColor: '#ddd',
+      margin: theme.spacing(2),
+      padding: theme.spacing(1),
+      ...theme.typography.h6
+    }
   })
 );
 
@@ -17,11 +40,24 @@ export type DevicePreviewProps = {
 const DevicePreview: React.FC<DevicePreviewProps> = ({ children }) => {
   const classes = useStyles();
   return (
-    <div style={{ padding: '2%', marginLeft: '200px'}}>
-        <img src={iPhone_XR} style={{ position: 'absolute'}} id='device-png'/>
-        <div style={{ position: 'absolute', marginTop: '132px', marginLeft: '35px'}} className={classes.appContainer}>
-          {children}
-        </div>
+    <div className={classes.root}>
+      <Grid container spacing={4}>
+
+        <Grid item xs={4} className={classes.gridLeft}>
+
+            <div className={classes.headline}>mosaic.mobi</div>
+            <div>coming soon to a phone near you.</div>
+
+        </Grid>
+
+        <Grid item xs={8}>
+                <img src={iPhone_XR} style={{ position: 'absolute'}} id='device-png'/>
+                <div style={{ position: 'absolute', marginTop: '132px', marginLeft: '35px'}} className={classes.appContainer}>
+                  {children}
+                </div>
+        </Grid>
+
+      </Grid>
     </div>
   );
 }
