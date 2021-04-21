34de594b168ca613ae086ff02aed57b956d2c5f0
Patrick S. Young <patrick@bigeyeball.co>
2021-04-21 15:38:03 -0700

    resolve conflict

--- mosaic-frontend/src/features/mosaicVideo/index.ts
+++ mosaic-frontend/src/features/mosaicVideo/index.ts
@@ -9,8 +9,5 @@ export type {
   MosaicPhase } from 'features/mosaicVideo/mosaicSlice';
 export { MosaicSelector } from 'features/mosaicVideo/mosaicSelector/MosaicSelector';
 export { MosaicTiles } from 'features/mosaicVideo/MosaicTiles';
-<<<<<<< HEAD
-=======
 export { VideoTex } from 'features/mosaicVideo/videoTex';
 export { GlTiles } from 'features/mosaicVideo/glTiles';
->>>>>>> MOS-94-webgl-rendering-on-webkit-gecko
