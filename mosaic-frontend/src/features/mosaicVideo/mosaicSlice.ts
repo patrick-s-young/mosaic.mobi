2baa47fa158051995f038c800b601435c53041ee
Patrick S. Young <patrick@bigeyeball.co>
2021-04-21 15:37:00 -0700

    resolve conflict

--- mosaic-frontend/src/features/mosaicVideo/mosaicSlice.ts
+++ mosaic-frontend/src/features/mosaicVideo/mosaicSlice.ts
@@ -2,14 +2,9 @@ import { createSlice, Draft, PayloadAction } from '@reduxjs/toolkit';
 import {
   getInPoints,
   getTileAnimEvents,
-<<<<<<< HEAD
-  getDrawToCanvasArea,
-  getCopyVideoFromArea
-=======
   getCopyVideoFromUvs,
   tileTranslation,
   tileVertices
->>>>>>> MOS-94-webgl-rendering-on-webkit-gecko
 }  from 'features/mosaicVideo/helpers';
 
 export enum MosaicPhaseEnum {
@@ -38,24 +33,15 @@ export type ActionGroupCollection = { [key in NumTilesToString] : ActionGroup };
 export type Time = number;
 export type TimeGroup = Array<Time>;
 export type TimeGroupCollection = { [key in NumTilesToString] : TimeGroup };
-<<<<<<< HEAD
-
-=======
 export type UvsCollection = { [key in NumTilesToString]: Array<number> };
 export type TileVertices = { [key in NumTilesToString] : Array<number> };
 export type TileTranslation = { [key in NumTilesToString] : Array<Array<number>> };
->>>>>>> MOS-94-webgl-rendering-on-webkit-gecko
 
 export interface MosaicFormatting {
   numTiles: NumTiles,
   canvasWidth: number,
   inPoints: TimeGroupCollection,
-<<<<<<< HEAD
-  copyVideoFromArea: RectCollection,
-  drawToCanvasArea: RectGroupCollection,
-=======
   copyVideoFromUvs: UvsCollection,
->>>>>>> MOS-94-webgl-rendering-on-webkit-gecko
   tileAnimEvents: ActionGroupCollection,
   tileTranslation: TileTranslation,
   tileVertices: TileVertices
@@ -69,15 +55,9 @@ const initialState: Partial<MosaicState> = {
   mosaicPhase: MosaicPhaseEnum.WAITING_FOR_VIDEO,
   canvasWidth: undefined,
   inPoints: undefined,
-<<<<<<< HEAD
-  copyVideoFromArea: undefined,
-  drawToCanvasArea: undefined,
-  tileAnimEvents: undefined
-=======
   tileAnimEvents: undefined,
   tileTranslation,
   tileVertices
->>>>>>> MOS-94-webgl-rendering-on-webkit-gecko
 }
 
 const mosaicSlice = createSlice({
@@ -93,10 +73,6 @@ const mosaicSlice = createSlice({
       state.copyVideoFromArea = getCopyVideoFromArea(videoWidth, videoWidth);
       state.tileAnimEvents = getTileAnimEvents();
       state.canvasWidth = canvasWidth;
-<<<<<<< HEAD
-      state.drawToCanvasArea = getDrawToCanvasArea(state.canvasWidth, state.canvasWidth);
-=======
->>>>>>> MOS-94-webgl-rendering-on-webkit-gecko
       state.numTiles = numTilesDefault;
       state.mosaicPhase = MosaicPhaseEnum.ANIMATION_STOPPED;
     },
