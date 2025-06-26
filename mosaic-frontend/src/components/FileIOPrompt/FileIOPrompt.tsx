0f6d30815822dd8758f054c351ca603ea858e16e
Patrick S. Young <patrick.young@pokeratlas.com>
2025-06-26 13:50:56 -0700

    MOS-140 jittery text in progress message

--- mosaic-frontend/src/components/FileIOPrompt/FileIOPrompt.tsx
+++ mosaic-frontend/src/components/FileIOPrompt/FileIOPrompt.tsx
@@ -8,7 +8,7 @@ import './fileIOPrompt.scss';
 let socket: Socket;
 
 const FileIOPrompt = ({ action, videoDuration = 0, callBack = () => {}, onCancel = () => {} }: FileIOPromptProps) => {
-  const [progressMessage, setProgressMessage] = useState('Processing frame 0 of 0');
+  const [progressFrames, setProgressFrames] = useState({currentFrame: 0, totalFrames: 0});
   const { headline, headlineIcon, message, buttonLabel } = PROMPT[action];
   const formattedMessage = action === 'videoTooLong' 
   ? message.replace('VIDEO_DURATION', Number(videoDuration).toFixed(2)) 
@@ -19,7 +19,7 @@ const FileIOPrompt = ({ action, videoDuration = 0, callBack = () => {}, onCancel
     socket = io();
     socket.on('ffmpegProgress', 
       (data: { currentFrame: number, totalFrames: number }) => {
-                  setProgressMessage(`Processing frame ${data.currentFrame} of ${data.totalFrames}`);
+                  setProgressFrames({currentFrame: data.currentFrame, totalFrames: data.totalFrames});
                 }
     );
   }, []);
@@ -30,7 +30,11 @@ const FileIOPrompt = ({ action, videoDuration = 0, callBack = () => {}, onCancel
         {headlineIcon} {headline}
       </div>
       <div>
-        {!showButtons && <p className='fileIOPrompt__progressMessage'>{progressMessage}</p>}
+        {!showButtons && 
+          <p className='fileIOPrompt__progressMessage'>
+            Processing frame <span style={{ display: "inline-block", width: "4ch", textAlign: "right" }}>{progressFrames.currentFrame}</span> of {progressFrames.totalFrames}
+          </p>
+        }
         {formattedMessage}
       </div>
       {showButtons &&
